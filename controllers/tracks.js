5175b395dcd95681b1de81c79b935da9ea64aecc
Stefano D'Annunzio <89990955+stefano-dannunzio@users.noreply.github.com>
2023-09-26 15:01:35 -0300

    Todos los endpoints de Recommendations finalizados

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -29,11 +29,17 @@ const getTrack = async (req = request, res = response) => {
 
 }
 
+let recommendations = [];
+
 const getRecommendations = async (req = request, res = response) => {
     const access_token = await getAuthFromClientCredentials();
     const { limit } = req.params;
     const { seed_genres, seed_artists } = req.query;
     const api_url = `https://api.spotify.com/v1/recommendations?limit=${limit}&seed_genres=${seed_genres}&seed_artists=${seed_artists}`;
+
+    if (recommendations.length > 0) {
+        recommendations = [];
+    }
 
     axios.get(api_url, {
         headers: {
@@ -45,7 +51,6 @@ const getRecommendations = async (req = request, res = response) => {
         console.log('LO SIGUIENTE ES EL ALBUM')
         console.log(data.data.tracks[0].album);
         const { tracks } = data.data;
-        const recommendations = [];
         let song_id = 1;
         tracks.forEach(track => {
             console.log(`------------------------------------------\nNombre de la pista: ${track.name}\nAlbum: ${track.album.name}\nArtista: ${track.artists[0].name}`);
@@ -69,6 +74,81 @@ const getRecommendations = async (req = request, res = response) => {
         });
     })
 }
+const getRecomById = (req = request, res = response) => {
+    const { id } = req.params;
+    if (id > recommendations.length) {
+        res.status(400).json({
+            status:400,
+            msg: `No existe la recomendación con ese ID, hay máximo ${recommendations.length} recomendaciones.`
+        });
+    }
+    try {
+        res.status(200).json(recommendations[id-1]);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            status:400,
+            msg: 'Error inesperado'
+        });
+        
+    }
+
+}
+
+const getRecomByArtistName = (req = request, res = response) => {
+    const { artist } = req.params;
+    let artist_recom = [];
+    recommendations.forEach(recom => {
+        if (recom.artist_name.toLowerCase().includes(artist.toLowerCase())) {
+            artist_recom.push(recom);
+        }
+    });
+    if (artist_recom.length == 0) {
+        res.status(400).json({
+            status:400,
+            msg: `No existe ninguna recomendación con ese artista.`
+        });
+    }
+    try {
+        res.status(200).json(artist_recom);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            status:400,
+            msg: 'Error inesperado'
+        });
+        
+    }
+    
+
+}
+
+const getRecomByAlbumName = (req = request, res = response) => {
+    const { album } = req.params;
+    let album_recom = [];
+    recommendations.forEach(recom => {
+        if (recom.album_name.toLowerCase().includes(album.toLowerCase())) {
+            album_recom.push(recom);
+        }
+    });
+    if (album_recom.length == 0) {
+        res.status(400).json({
+            status:400,
+            msg: `No existe ninguna recomendación con ese album.`
+        });
+    }
+    try {
+        res.status(200).json(album_recom);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            status:400,
+            msg: 'Error inesperado'
+        });
+        
+    }
+
+}
 
 
-module.exports = {getTrack, getRecommendations};
+module.exports = {getTrack, getRecommendations, getRecomById, getRecomByArtistName, getRecomByAlbumName};
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
-const { getTrack, getRecommendations } = require('../controllers/tracks');
+const { getTrack, getRecommendations, getRecomById, getRecomByArtistName, getRecomByAlbumName } = require('../controllers/tracks');
 
 const rutas = Router();
 //RUTA DE REFERENCIA
 rutas.get('/:id', getTrack);
 
 //GetRecommendations - STEFA
-rutas.get('/recommendations/:limit', getRecommendations);
+rutas.get('/recommendations/limit/:limit', getRecommendations);
+rutas.get('/recommendations/id/:id', getRecomById);
+rutas.get('/recommendations/artist/:artist', getRecomByArtistName);
+rutas.get('/recommendations/album/:album', getRecomByAlbumName);
 
 module.exports = rutas;
