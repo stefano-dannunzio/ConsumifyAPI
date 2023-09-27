20de23fcb98b429416c6ddc9136acccd4fe90f46
Mauro Sagasti <maurosagasti@gmail.com>
2023-09-27 20:20:21 -0300

    Ultimos arreglos

--- routes/tracks.js
+++ routes/tracks.js
@@ -7,9 +7,5 @@ rutas.get('/:id', getTrack);
 
 //GetRecommendations - STEFA
 
-// rutas.get('/recommendations/limit/:limit', getRecommendations);
-// rutas.get('/recommendations/id/:id', getRecomById);
-// rutas.get('/recommendations/artist/:artist', getRecomByArtistName);
-// rutas.get('/recommendations/album/:album', getRecomByAlbumName);
 
 module.exports = rutas;
