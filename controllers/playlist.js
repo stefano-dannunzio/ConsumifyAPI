fdbb9f909faad450eb8f531227b2cd0b3810984a
Maximo Ozonas <maxiozonas10@gmail.com>
2023-09-26 23:23:35 -0300

    Agrego filtro por ID del artista

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const {request, response} = require('express');
 const getAuthFromClientCredentials = require('../services/client_credentials_auth')
 
+
 const getPlaylist = async (req = request, res = response) => {
     try {
         const token = await getAuthFromClientCredentials();
@@ -14,25 +15,98 @@ const getPlaylist = async (req = request, res = response) => {
             },
         });
 
-        console.log('Respuesta de Axios:', response.data);
-        
-        if (response.status === 200) {
-            res.status(200).json(response.data);
-        } else if (response.status === 401) {
-            res.status(401).json({ message: 'Solicitud no autorizada.' })
-        } else if (response.status === 403) {
-            res.status(403).json ({ message: "Solicitud prohibida." })
-        } else if (response.status === 404) {
-            res.status(404).json ({ message: 'Playlist no encontrada.' })
-        } else {
-            res.status(response.status).json({ message: 'Error en la solicitud.' })
-        }
 
+        if (response.status === 200) {
+            const responseData = { ...response.data };
+            
+            // Extraer información específica de la respuesta para personalizarla
+            const playlistInfo = {
+                name: responseData.name,
+                createdBy: responseData.owner.display_name,
+                createdAt: responseData.snapshot_id.split('T')[0], // Fecha de creación
+                tracks: [],
+            };
+
+            // Recorrer las canciones y obtener el nombre de la canción y el nombre del artista
+            responseData.tracks.items.forEach((item) => {
+                const trackInfo = {
+                    name: item.track.name,
+                    artist: item.track.artists.map((artist) => artist.name).join(', '), // Obtener el nombre del artista
+                };
+                playlistInfo.tracks.push(trackInfo);
+            });
+
+            res.status(200).json(playlistInfo);
+        } else if (response.status === 401) {
+            res.status(401).json({ message: 'Solicitud no autorizada.' });
+        } else if (response.status === 403) {
+            res.status(403).json({ message: 'Solicitud prohibida.' });
+        } else if (response.status === 404) {
+            res.status(404).json({ message: 'Playlist no encontrada.' });
+        } else {
+            res.status(response.status).json({ message: 'Error en la solicitud.' });
+        }
     } catch (error) {
-        console.log(error);
+        console.log('Error en getPlaylist:', error);
         res.status(500).json({ message: 'Error interno del servidor' });
     }
 };
 
-module.exports = { getPlaylist };
+
+const getArtistById = async (req = request, res = response) => {
+    try {
+        const token = await getAuthFromClientCredentials();
+        console.log('Token obtenido: ', token);
+        const { playlistId } = req.params;
+        const artistId = req.query.artistId; // Obtener el valor del query parameter 'artistId'
+
+        console.log('Artista ID:', artistId);
+
+        const response = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+
+
+        if (response.status === 200) {
+            const responseData = { ...response.data };
+            const filteredTracks = artistId
+                ? response.data.tracks.items.filter(item => item.track.artists.some(a => a.id === artistId))
+                : response.data.tracks.items;
+
+            // Actualizar la propiedad 'tracks' en la respuesta con los tracks filtrados
+            responseData.tracks.items = filteredTracks;
+
+            // Devolver la respuesta actualizada
+                const respuestaModificada = {
+                name: responseData.name,
+                creator: responseData.owner.display_name,
+                created_at: responseData.snapshot_id, // Esto puede no ser la fecha de creación exacta, verifica si Spotify proporciona esta información
+                tracks: filteredTracks.map(track => ({
+                    name: track.track.name,
+                    artist: track.track.artists.map(artist => artist.name).join(', '), // Para obtener todos los artistas de la canción
+                })),
+            };
+
+            res.status(200).json(respuestaModificada);
+        } else if (response.status === 401) {
+            res.status(401).json({ message: 'Solicitud no autorizada.' });
+        } else if (response.status === 403) {
+            res.status(403).json({ message: "Solicitud prohibida." });
+        } else if (response.status === 404) {
+            res.status(404).json({ message: 'Playlist no encontrada.' });
+        } else {
+            res.status(response.status).json({ message: 'Error en la solicitud.' });
+        }
+    } catch (error) {
+        console.log('Error en getArtistById:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+};
+
+
+
+
+module.exports = { getPlaylist, getArtistById };
 
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,8 +1,12 @@
-const {Router} = require('express');
-const { getPlaylist } = require('../controllers/playlist');
+const { Router } = require('express');
+const { getPlaylist, getArtistById } = require('../controllers/playlist');
 
 const rutas = Router();
 
+// Ruta para obtener la playlist por ID
 rutas.get('/:playlistId', getPlaylist);
+
+// Ruta para filtrar la playlist por artista
+rutas.get('/:playlistId/artistId', getArtistById);
 
 module.exports = rutas;
